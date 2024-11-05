80adadc9d3976f5751108c2b97af4fd82faf960d
TranKietHCMUS <anhkiet10h1nd@gmail.com>
2024-11-05 12:25:21 +0700

    add button class

--- utils/Button.py
+++ utils/Button.py
@@ -14,7 +14,8 @@ class Button():
         pass
 
     def draw(self, screen):
+        rect = pygame.Rect(self.x, self.y, self.height, self.width)
         text_surface = UIConfig.STATS_FONT.render(self.text, True, (255, 255, 255))
-        text_rect = text_surface.get_rect(center=self.rect.center)
+        text_rect = text_surface.get_rect(center=rect.center)
         pygame.draw.rect(screen, self.color, self.rect)  # Vẽ nút
         screen.blit(text_surface, text_rect)
